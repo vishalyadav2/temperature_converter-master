8965319b77ecc4b3180798220a83f7e33e07db8a
vishalyadav2 <43594358+vishalyadav2@users.noreply.github.com>
2021-03-21 20:34:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,25 @@
-from flask import Flask, render_template, redirect, url_for, session
-from flask_bs4 import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '...'
-bs4 = Bootstrap(app)
-
-
-class ConvertForm(FlaskForm):
-    fahrenheit = IntegerField('Fahrenheit:')
-    submit = SubmitField('Submit')
-
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
+    return render_template('index.html')
 
+@app.route('/result', methods=['POST'])
+def result():
+    var_1 = request.form.get("var_1", type=int)
+    
+    operation = request.form.get("operation")
+    if(operation == 'Fahrenheit'):
+        result = (var_1 * 9/5) + 32
+     elif(operation == 'Celcius'):
+        result = (var_1 – 32) * 5/9
+    else:
+        result = 'INVALID CHOICE'
+    entry = result
+    return render_template('result.html', entry=entry)
 
-@app.route('/convert', methods=['GET', 'POST'])
-def convert():
-    """formula C = 5/9 * (F -32) Fahrenheit to Celsius"""
-    table = []
-    for f in range(30, 101, 2):
-        c = 5 / 9 * (f -32) 
-        c = round(c, 1)
-        table.append((f, c))
+if __name__ == "__main__":
+    app.run(debug=True)
 
-    form = ConvertForm()
-
-    if form.validate_on_submit():
-        f = form.fahrenheit.data
-        c = 5 / 9 * (f -32)
-        c = round(c, 1)
-        session['fa'] = f
-        session['celsius'] = c
-        form.fahrenheit.data = ''
-        return redirect(url_for('convert'))
-
-    return render_template('convert.html', table=table, form=form, 
-        f = session.get('fa', ''), 
-        c=session.get('celsius', ''))
